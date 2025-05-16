fbcf74f7ea1d10801cd955c9bc67ea4dab7342e6
Fuad-Delil <mahidelil111@gmail.com>
2025-05-16 07:09:49 +0300

    Banner image created

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-
-import './App.css'
-import Home from './Pages/Home/Home'
+import "./App.css";
+import Home from "./Pages/Home/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,55 +1,15 @@
-import React from 'react'
- import Header from "./../../Components/header/Header"
- import Footer from "./../../Components/Footer/Footer"
+import React from "react";
+import Header from "./../../Components/header/Header";
+import Footer from "./../../Components/Footer/Footer";
+import Banner from "../../Components/Banner/Banner";
 const Home = () => {
   return (
     <>
       <Header />
-      <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi aut
-        distinctio quibusdam consequuntur! Inventore iure illum ab qui?
-        Assumenda officia veritatis, corrupti delectus reiciendis repellendus
-        labore commodi eaque voluptatibus officiis.
-      </div>
-      <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi aut
-        distinctio quibusdam consequuntur! Inventore iure illum ab qui?
-        Assumenda officia veritatis, corrupti delectus reiciendis repellendus
-        labore commodi eaque voluptatibus officiis.
-      </div>
-      <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi aut
-        distinctio quibusdam consequuntur! Inventore iure illum ab qui?
-        Assumenda officia veritatis, corrupti delectus reiciendis repellendus
-        labore commodi eaque voluptatibus officiis.
-      </div>
-      <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi aut
-        distinctio quibusdam consequuntur! Inventore iure illum ab qui?
-        Assumenda officia veritatis, corrupti delectus reiciendis repellendus
-        labore commodi eaque voluptatibus officiis.
-      </div>
-      <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi aut
-        distinctio quibusdam consequuntur! Inventore iure illum ab qui?
-        Assumenda officia veritatis, corrupti delectus reiciendis repellendus
-        labore commodi eaque voluptatibus officiis.
-      </div>
-      <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi aut
-        distinctio quibusdam consequuntur! Inventore iure illum ab qui?
-        Assumenda officia veritatis, corrupti delectus reiciendis repellendus
-        labore commodi eaque voluptatibus officiis.
-      </div>
-      <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi aut
-        distinctio quibusdam consequuntur! Inventore iure illum ab qui?
-        Assumenda officia veritatis, corrupti delectus reiciendis repellendus
-        labore commodi eaque voluptatibus officiis.
-      </div>
+      <Banner />
       <Footer />
     </>
   );
-}
+};
 
-export default Home
+export default Home;
